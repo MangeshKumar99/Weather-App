c6a2390d3b989ed870156ef1c2a8bf0166425b18
Mangesh Kumar <m1064413@mindtree.com>
2021-04-20 11:24:36 +0530

    first commit

---
+++ src/app/weather.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  option:any
+
+  constructor(private http: HttpClient) { 
+    console.log("Service is now ready")
+  }
+  getCurrentWeather(option:any){
+    return this.http.get("http://api.weatherstack.com/current?access_key=f915a74916953e30c822d76229b4adfc&query="+option+"") 
+    .pipe(map((res) => res));
+  }
+  getWeather(latitude:any,longitude:any){
+    return this.http.get(`http://api.weatherstack.com/current?access_key=f915a74916953e30c822d76229b4adfc&query=${latitude},${longitude}`) 
+    .pipe(map((res) => res));
+  }
+  getTimely(name:any){
+    return this.http.get("https://api.openweathermap.org/data/2.5/forecast?q="+name+"&appid=904f4892e202e4b9e33580d6b6f2cd52") 
+    .pipe(map((res) => res));
+  }
+
+}
