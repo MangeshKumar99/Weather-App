22e084049d51aec366b4a60ce746e50bf41d1292
Mangesh <mangesh2599@gmail.com>
2022-03-13 15:23:14 +0530

    ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { CurrentComponent } from './current/current.component';
-import { ForecastComponent } from './forecast/forecast.component';
 import {WeatherService} from './weather.service'
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     CurrentComponent,
-    ForecastComponent
   ],
   imports: [
     BrowserModule,
--- src/app/current/current.component.ts
+++ src/app/current/current.component.ts
@@ -4,86 +4,47 @@ import { WeatherService } from '../weather.service';
 @Component({
   selector: 'app-current',
   templateUrl: './current.component.html',
-  styleUrls: ['./current.component.css']
+  styleUrls: ['./current.component.css'],
 })
 export class CurrentComponent implements OnInit {
+  place: any;
+  myCity: any;
+  x: any;
+  y: any;
+  newData: any;
+  oldData: any;
+  reload: any;
+  latitude: any;
+  longitude: any;
+  weather: any;
+  forecast: any;
+  isSearched:boolean=false;
+  forecasting: Array<any> = [];
 
-  place: any
-  myCity: any
-  x: any
-  y: any
-  newData: any
-  oldData: any
-  reload: any
-  latitude: any
-  longitude: any
-  weather:any
-  forecast:any
-  forecasting:Array<any>=[]
-
-
-
-
-  constructor(private ws: WeatherService) { }
+  constructor(private ws: WeatherService) {}
 
   ngOnInit(): void {
-    this.getCurrentLocation()
+    // this.getCurrentLocation();
+  }
+  // getCurrentLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //     });
+  //     console.log(this.latitude,this.longitude)
+  //   }
+  //   this.ws.getWeather(this.latitude, this.longitude).subscribe((pos) => {
+  //     this.weather = pos;
+  //     console.log(this.weather);
+  //   });
+  // }
   
-  }
-  getCurrentLocation(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(position=>{
-        this.latitude=position.coords.latitude
-        this.longitude=position.coords.longitude
-      })
-    }
-  }
-
   findData(myCity: any) {
-    this.ws.getCurrentWeather(myCity).subscribe(place => {
-      console.log(place)
-      this.place = place
-    })
-    this.ws.getWeather(this.latitude,this.longitude).subscribe(pos=>{
-      this.weather=pos
-      console.log(this.weather)
-    })
+    this.isSearched=true;
+    this.ws.getCurrentWeather(myCity).subscribe((place) => {
+      console.log(place);
+      this.place = place;
+    });
   }
-  myFunction1() {
-    this.x = document.getElementById("mySelect");
-    this.x.remove(this.x.selectedIndex);
-  }
-  myFunction2(choice: any) {
-    this.y = document.getElementById("mySelect");
-    var option = document.createElement("option");
-    option.text = choice;
-    this.y.add(option);
-  }
-  save(s: any) {
-    localStorage.setItem("city", s);
-  }
-  retrieve() {
-    this.reload = localStorage.getItem("city");
-    this.myFunction2(this.reload)
-  }
- getForecast(opt:any){
-   console.log(opt)
-   this.ws.getTimely(opt).subscribe(forecast=>{
-     console.log(forecast)
-     this.forecast=forecast
-     for(let i=0;i<this.forecast.list.length;i++){
-       if(i%8==0){
-         console.log(this.forecast.list[i])
-         this.forecasting.push(this.forecast.list[i])
-       }
-     }
-     console.log(this.forecasting)
-   })
- }
-
-
-
-
-
-
 }
